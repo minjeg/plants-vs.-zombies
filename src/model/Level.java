28958eab4ec8a1954a1220cfdaa48b79d38d53b1
minjeg <122382943+minjeg@users.noreply.github.com>
2024-12-28 17:14:38 +0800

    优化关卡系统

--- src/model/Level.java
+++ src/model/Level.java
@@ -13,9 +13,9 @@ public class Level implements Serializable {
     private final int initialSun;
     private int currentWave = 0;
     private final int totalWave;
-    private boolean isFlagWave = false;
     private long currentTime = 0, totalTime = 18000;
-    private int waveTotalZombieHealth;
+    private int currentWaveTotalZombieHealth, nextWaveTotalZombieHealth;
+    private List<Zombie> nextWaveZombies;
 
     private final List<Double> rowWeight;
     private final List<Integer> lastPicked;
@@ -58,17 +58,24 @@ public class Level implements Serializable {
             lastPicked.add(0);
             secondLastPicked.add(0);
         }
+        nextWaveZombies = decideZombies(currentWave);
+        nextWaveTotalZombieHealth = 0;
+        for (Zombie zombie : nextWaveZombies)
+            nextWaveTotalZombieHealth += zombie.getHealth();
     }
 
     public void update(GameModel gameModel) {
+        if (currentWave != 0 && gameModel.getTotalZombieHealth() == 0) {
+            gameModel.setState(GameModel.State.WIN);
+            return;
+        }
         if (currentTime >= totalTime) {
-            currentTime = 0;
-            isFlagWave = currentWave % 10 == 9 || totalWave < 10 && currentWave == totalWave - 1;
+            currentTime -= totalTime;
             if (currentWave > 4 && currentWave < 25) {
                 zombieTypes.getFirst().setWeight(zombieTypes.getFirst().getWeight() - 180);
                 zombieTypes.get(1).setWeight(zombieTypes.get(1).getWeight() - 150);
             }
-            if (isFlagWave)
+            if (isFlagWave(currentWave))
                 totalTime = 55000;
             else if (currentWave % 10 == 8 || currentWave == totalWave - 2)
                 totalTime = 45000;
@@ -76,22 +83,26 @@ public class Level implements Serializable {
                 totalTime = (long) ((25 + Math.random() * 6) * 1000);
             if (currentWave < totalWave) {
                 System.out.println("第" + (currentWave + 1) + "波");
-                if (isFlagWave) {
+                if (isFlagWave(currentWave)) {
                     System.out.println("一大波僵尸正在接近！");
                     if (currentWave == totalWave - 1)
                         System.out.println("最后一波！");
                 }
-                new Timer().schedule(new TimerTask() {
+                new Timer().scheduleAtFixedRate(new TimerTask() {
                     private List<Zombie> zombies;
                     private int index = 0;
 
                     @Override
                     public void run() {
                         if (zombies == null) {
-                            zombies = decideZombies();
-                            waveTotalZombieHealth = 0;
-                            for (Zombie zombie : zombies)
-                                waveTotalZombieHealth += zombie.getHealth();
+                            zombies = nextWaveZombies;
+                            currentWaveTotalZombieHealth = nextWaveTotalZombieHealth;
+                            new Thread(() -> {
+                                nextWaveZombies = decideZombies(currentWave + 1);
+                                nextWaveTotalZombieHealth = 0;
+                                for (Zombie zombie : nextWaveZombies)
+                                    nextWaveTotalZombieHealth += zombie.getHealth();
+                            }).start();
                         } else if (index == zombies.size()) {
                             ++currentWave;
                             this.cancel();
@@ -101,27 +112,12 @@ public class Level implements Serializable {
                         }
                     }
                 }, 0, 500);
-            } else {
-                if (gameModel.getTotalZombieHealth() == 0)
-                    gameModel.setState(GameModel.State.WIN);
             }
         } else {
-            if (currentTime > 4000 && currentWave != 0 && 2 * gameModel.getTotalZombieHealth() <= waveTotalZombieHealth && totalTime - currentTime > 2000)
+            if (currentTime > 4000 && currentWave != 0 && 2 * gameModel.getTotalZombieHealth() <= currentWaveTotalZombieHealth && totalTime - currentTime > 2000)
                 currentTime = totalTime - 2000;
             currentTime += gameModel.getUpdateGap();
         }
-    }
-
-    public int getInitialSun() {
-        return initialSun;
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public int getCols() {
-        return cols;
     }
 
     /// 确定僵尸出现的行
@@ -152,22 +148,22 @@ public class Level implements Serializable {
     }
 
     /// 确定一波的僵尸
-    private List<Zombie> decideZombies() {
+    private List<Zombie> decideZombies(int wave) {
         int levelSum = 0;
         int zombieTypeNumber = zombieTypes.size();
-        List<Zombie> zombies = new ArrayList<>(getLevelUpperLimit());
+        List<Zombie> zombies = new ArrayList<>(getLevelUpperLimit(wave));
         int i;
-        if (isFlagWave) {
-            levelSum = 1 + Math.min(getOriginalLevelUpperLimit(), 8);
+        if (isFlagWave(wave)) {
+            levelSum = 1 + Math.min(getOriginalLevelUpperLimit(wave), 8);
             for (i = 1; i < levelSum; ++i)
                 zombies.add(new BasicZombie());
         }
-        while (levelSum < getLevelUpperLimit()) {
+        while (levelSum < getLevelUpperLimit(wave)) {
             List<Integer> allowedZombieTypes = new ArrayList<>(zombieTypeNumber);
             List<Integer> zombieWeight = new ArrayList<>(zombieTypeNumber);
             for (i = 1; i < zombieTypeNumber; ++i) {
                 ZombieType zombieType = zombieTypes.get(i);
-                if (levelSum + zombieType.getLevel() <= getLevelUpperLimit()) {
+                if (levelSum + zombieType.getLevel() <= getLevelUpperLimit(wave)) {
                     allowedZombieTypes.add(i);
                     if (zombieWeight.isEmpty())
                         zombieWeight.add(zombieType.getWeight());
@@ -193,29 +189,45 @@ public class Level implements Serializable {
         return zombies;
     }
 
+    /// 获得波次的原始级别上限
+    private int getOriginalLevelUpperLimit(int wave) {
+        int number;
+        if (wave < 20)
+            number = wave / 3 + 1;
+        else
+            number = (int) (0.4 * wave + 1);
+        return number;
+    }
+
+    /// 获取波次的级别上限
+    private int getLevelUpperLimit(int wave) {
+        int number = getOriginalLevelUpperLimit(wave);
+        if (isFlagWave(wave))
+            number = (int) (number * 2.5);
+        return number;
+    }
+
+    private boolean isFlagWave(int wave) {
+        return wave % 10 == 9 || wave < 10 && wave == totalWave - 1;
+    }
+
+    public int getInitialSun() {
+        return initialSun;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
     public int getTotalWave() {
         return totalWave;
     }
 
     public int getCurrentWave() {
         return currentWave;
-    }
-
-    /// 获得当前波次的原始级别上限
-    private int getOriginalLevelUpperLimit() {
-        int number;
-        if (currentWave < 20)
-            number = currentWave / 3 + 1;
-        else
-            number = (int) (0.4 * currentWave + 1);
-        return number;
-    }
-
-    /// 获取当前波次的级别上限
-    private int getLevelUpperLimit() {
-        int number = getOriginalLevelUpperLimit();
-        if (isFlagWave)
-            number = (int) (number * 2.5);
-        return number;
     }
 }
